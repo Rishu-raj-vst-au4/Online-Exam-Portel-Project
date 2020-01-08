803cd72951674497559db5dbe1ba45cc5accce9a
Ganesh Dasari <gdasari74@gmail.com>
2020-01-08 14:54:13 +0530

    bug fix update
    bug fix update

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const port = 9090;
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static('public'))
-
-const controller = require('./Controller/controller');
-console.log(controller);
+app.use(express.static("public"));
 
 // Requireing and registiering the express-handlebars:
-
-app.engine('.hbs', exphbs());
-app.set('view engine', '.hbs');
-
-
-app.get('/', function(req, res) {
-    res.render('Landing');
-})
-app.get('/signup', function(req, res) {
-    res.render('signup');
-})
-app.post('/signup', controller.add);
-
-app.listen(port, function() {
-    console.log("App is at port", port)
+const hbs = exphbs.create({
+  extname: ".hbs"
 });
+
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+
+//configure
+const controller = require("./Controller/controller");
+console.log(controller);
+
+//Routes
+
+app.get("/", function(req, res) {
+  res.render("Landing");
+});
+app.get("/signup", function(req, res) {
+  res.render("signup");
+});
+app.post("/signup", controller.add);
+
+//PORT
+app
+  .listen(port, function() {
+    console.log("Application has started and running on port: ", port);
+  })
+  .on("error", function(error) {
+    console.log("Unable to start app. Error >>>>", error);
+  });
--- public/Js/signup.js
+++ public/Js/signup.js
@@ -1,32 +1,29 @@
 $(document).ready(function() {
-    $('#Agreeandjoin').on('click', function() {
-        let firstname = $('#firstname').val();
-        let lastname = $('#last-name').val();
-        let gender = $('#gender').val();
-        let email = $('#inputEmail').val();
-        let mobilenumber = $('#Mobile-number').val();
-        let password = $('#password').val();
-        let confirmpassword = $('#Confirm Password').val();
-        console.log(firstname)
-        $.ajax({
-            method: "POST",
-            url: '/signup',
-            dataType: 'json',
-            data: {
-                Firstname: firstname,
-                Lastname: lastname,
-                Gender: gender,
-                Email: email,
-                Mobilenumber: mobilenumber,
-                Password: password,
-                ConfirmPassword: confirmpassword
-            },
-            success: function() {
-
-            },
-            error: function() {
-
-            }
-        });
+  $("#Agreeandjoin").on("click", function() {
+    console.log("clicked");
+    let firstname = $("#firstname").val();
+    let lastname = $("#last-name").val();
+    let gender = $("#gender").val();
+    let email = $("#inputEmail").val();
+    let mobilenumber = $("#Mobile-number").val();
+    let password = $("#password").val();
+    let confirmpassword = $("#Confirm Password").val();
+    console.log(firstname);
+    $.ajax({
+      method: "POST",
+      url: "/signup",
+      dataType: "json",
+      data: {
+        Firstname: firstname,
+        Lastname: lastname,
+        Gender: gender,
+        Email: email,
+        Mobilenumber: mobilenumber,
+        Password: password,
+        ConfirmPassword: confirmpassword
+      },
+      success: function() {},
+      error: function() {}
     });
+  });
 });
